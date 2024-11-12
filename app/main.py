255aa044312c3d175bebc792346b4aca1a69a6b0
Oleh <sholeg2005@gmail.com>
2024-11-12 19:09:40 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,55 +5,64 @@ import math
 
 class Vector:
 
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_c: float, y_c: float) -> None:
+        self.x_c = round(x_c, 2)
+        self.y_c = round(y_c, 2)
 
-    def __add__(self, vector1: "Vector") -> "Vector":
-        return Vector(vector1.x + self.x, vector1.y + self.y)
+    @property
+    def x(self):
+        return self.x_c
 
-    def __sub__(self, vector1: "Vector") -> "Vector":
-        return Vector(self.x - vector1.x, self.y - vector1.y)
+    @property
+    def y(self):
+        return self.y_c
 
-    def __mul__(self, other: Union[int, float, "Vector"])\
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(other.x_c + self.x_c, other.y_c + self.y_c)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x_c - other.x_c, self.y_c - other.y_c)
+
+    def __mul__(self, other: Union[int, float, "Vector"]) \
             -> Union[float, "Vector"]:
         if isinstance(other, (int, float)):
-            return Vector(self.x * other, self.y * other)
+            return Vector(self.x_c * other, self.y_c * other)
         elif isinstance(other, Vector):
-            return (self.x * other.x) + (self.y * other.y)
+            return (self.x_c * other.x_c) + (self.y_c * other.y_c)
 
     @classmethod
     def create_vector_by_two_points(cls, stat: tuple, en: tuple) -> "Vector":
         return cls(en[0] - stat[0], en[1] - stat[1])
 
     def get_length(self) -> float:
-        return sqrt(pow(self.x, 2) + pow(self.y, 2))
+        return sqrt(pow(self.x_c, 2) + pow(self.y_c, 2))
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
             raise ValueError("Cannot normalize a zero-length vector")
-        return Vector(self.x / length, self.y / length)
+        return Vector(self.x_c / length, self.y_c / length)
 
     def angle_between(self, other: "Vector") -> float:
-        upper_mul = self * other
-        length_mul = self.get_length() * other.get_length()
+        dot_product = self * other
+        lengths_product = self.get_length() * other.get_length()
 
-        if length_mul == 0:
+        if lengths_product == 0:
             raise ValueError("Error")
 
-        cos_res = upper_mul / length_mul
-        res = round(math.degrees(math.acos(cos_res)))
+        cos_theta = dot_product / lengths_product
+        angle = round(math.degrees(math.acos(cos_theta)))
 
-        return res
+        return angle
 
     def get_angle(self) -> float:
-        before = self.y / self.get_length()
-        return round(math.degrees(math.acos(before)))
+        cos_theta = self.y_c / self.get_length()
+        return round(math.degrees(math.acos(cos_theta)))
 
-    def rotate(self, number: int) -> "Vector":
-        r_num = math.radians(number)
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
 
-        x_rotate = self.x * math.cos(r_num) - self.y * math.sin(r_num)
-        y_rotate = self.x * math.sin(r_num) + self.y * math.cos(r_num)
-        return Vector(round(x_rotate, 2), round(y_rotate, 2))
+        x_rot = self.x_c * math.cos(radians) - self.y_c * math.sin(radians)
+        y_rot = self.x_c * math.sin(radians) + self.y_c * math.cos(radians)
+
+        return Vector(round(x_rot, 2), round(y_rot, 2))
